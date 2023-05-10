f47469b39393c6bf54b1447fed21dbaea3b0667a
Julian Kozlowski <julian.kozlowski99@gmail.com>
2023-05-10 13:39:21 +0200

    Updating dotfiles

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,44 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("", "<leader>-", ":split<CR><C-w>j")
+vim.keymap.set("", "<leader>c", "<C-w>c")
+
+vim.keymap.set("", "<C-h>", "<C-w>h")
+vim.keymap.set("", "<C-j>", "<C-w>j")
+vim.keymap.set("", "<C-k>", "<C-w>k")
+vim.keymap.set("", "<C-l>", "<C-w>l")
+
+vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Save" })
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+vim.keymap.set({ "n", "x" }, "cp", '"+y')
+vim.keymap.set({ "n", "x" }, "cv", '"+p')
+vim.keymap.set({ "n", "x" }, "x", '"_x')
+vim.keymap.set("", "<leader>/", ":vsplit<CR><C-w>l")
+
+-- mappings for telescope fuzzy finding
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', function() 
+	builtin.grep_string({search = vim.fn.input("Grep > ")});
+end)
+
+-- mappings for LSP
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,54 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use 'nvim-treesitter/nvim-treesitter'
+
+	require'nvim-treesitter.configs'.setup {
+		ensure_installed = { "c", "lua", "vim", "vimdoc", "query"},
+		sync_install = false,
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+	}
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      {'neovim/nvim-lspconfig'},
+      {
+      'williamboman/mason.nvim',
+      run = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'},
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
+   }
+  }
+  local lsp = require('lsp-zero').preset({})
+
+  lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({buffer = bufnr})
+  end)
+
+  require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+  lsp.setup()
+
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+  vim.cmd('colorscheme rose-pine')
+
+	use 'christoomey/vim-tmux-navigator'
+
+end)
